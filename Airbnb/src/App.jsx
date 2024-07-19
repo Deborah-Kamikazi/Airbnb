4686d6201a111564483aa45161150abfd52ce4b8
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-07-19 10:55:07 +0200

    start of the project

---
+++ Airbnb/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section className='flex flex-col items-center mt-3'>
+     <nav className=' bg-[#FFFFFF] h-[70px] w-[550px] drop-shadow-md flex items-center'> 
+         <img src="/airbnb 1.svg" alt="" className='px-2'/> 
+     </nav>
+     <div className='bg-[#FFFFFF]  w-[550px] bg-slate-100'>
+       <img src="/Group 77.svg" alt="" className='px-10 mt-8' />
+       <h1 className='text-3xl font-bold mt-8 px-7'>Online Experiences </h1>
+       <div className='leading-relaxed font-normal text-sm mt-3 px-7'>
+  <span className='block'>Join unique interactive activities led by</span>
+  <span className='block'>one-of-a-kind hosts—all without leaving</span>
+  <span className='block'>home.</span>
+</div>
+
+     </div>
+     
+     </section>
+    </>
+  )
+}
+
+export default App
